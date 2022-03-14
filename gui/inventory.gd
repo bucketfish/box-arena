0288171896d91dd92982caf7c72756e28ce11576
bucketfishy <bucketfishy@gmail.com>
2022-03-14 11:34:14 +0800

    inventory focus

--- gui/inventory.gd
+++ gui/inventory.gd
@@ -1,17 +1,27 @@
 extends Control
 
 onready var shade = $"../shade"
+onready var rowbox = $scroll/rowbox
 
 onready var row = [$scroll/rowbox/row, $scroll/rowbox/row2, $scroll/rowbox/row3, $scroll/rowbox/row4]
+
+var row_scene = preload("res://gui/row.tscn")
+
+onready var curhighlight = $scroll/rowbox/row/item
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	visible = false
+	connect_rows()
 
 func toggle():
 	visible = !visible
 	shade.visible = visible
 	display(Persistent.carrying)
+	
+	curhighlight.focused = false
+	row[0].col[0].focused = true
+	curhighlight = row[0].col[0]
 	
 func display(items):
 	var items_dict = Functions.sort_inventory(items)
@@ -27,4 +37,40 @@ func display(items):
 			colc = 0
 			rowc += 1
 			
+		if rowc >= row.size():
+			var newrow = row_scene.instance()
+			rowbox.add_child(newrow)
+			row.append(newrow)
+			connect_rows()
+			
 	get_tree().call_group("inv_item", "prep_display")
+
+func connect_rows():
+	for currow in range(0, row.size()):
+		for curbox in range(0, 4):
+			var box = row[currow].col[curbox]
+			
+			if currow != 0:
+				box.up = row[currow-1].col[curbox]
+			if currow != row.size()-1:
+				box.down = row[currow+1].col[curbox]
+
+func _input(event):
+	if visible == true:
+		if Input.is_action_just_pressed("ui_up") && curhighlight.up:
+			curhighlight.focused = false
+			curhighlight.up.focused = true
+			curhighlight = curhighlight.up
+		elif Input.is_action_just_pressed("ui_down") && curhighlight.down:
+			curhighlight.focused = false
+			curhighlight.down.focused = true
+			curhighlight = curhighlight.down
+		elif Input.is_action_just_pressed("ui_left") && curhighlight.left:
+			curhighlight.focused = false
+			curhighlight.left.focused = true
+			curhighlight = curhighlight.left
+		elif Input.is_action_just_pressed("ui_right") && curhighlight.right:
+			curhighlight.focused = false
+			curhighlight.right.focused = true
+			curhighlight = curhighlight.right
+		
--- gui/item.gd
+++ gui/item.gd
@@ -11,16 +11,45 @@ onready var count = $count
 var itemname
 var countint = 1
 
-export var left:NodePath
-export var right:NodePath
-export var up:NodePath
-export var down:NodePath
+export var left_path:NodePath setget setnode_left
+export var right_path:NodePath setget setnode_right
+export var up_path:NodePath setget setnode_up
+export var down_path:NodePath setget setnode_down
+
+var left
+var right
+var up
+var down
 
 var focused = false setget highlight
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	highlight(false) # Replace with function body.
+	setnode_left(left_path)
+	setnode_right(right_path)
+	setnode_up(up_path)
+	setnode_down(down_path)
+	
+func setnode_left(path):
+	if path:
+		left = get_node(path)
+		left_path = path
+	
+func setnode_right(path):
+	if path:
+		right = get_node(path)
+		right_path = path
+		
+func setnode_down(path):
+	if path:
+		down = get_node(path)
+		down_path = path
+	
+func setnode_up(path):
+	if path:
+		up = get_node(path)
+		up_path = path
 	
 func prep_display():
 	if itemname:
@@ -34,4 +63,3 @@ func prep_display():
 func highlight(value):
 	highlight.visible = value
 	focused = value
-

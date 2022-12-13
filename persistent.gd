a62f1721c901b20fbe3e3d8aded9df868371cddf
bucketfishy <bucketfishy@gmail.com>
2022-12-13 13:02:13 +0800

    balancing energy

--- persistent.gd
+++ persistent.gd
@@ -5,7 +5,7 @@ onready var base = get_node("/root/base")
 
 var endgames = []
 
-var coords = Vector2(-9, -9)
+var coords = Vector2(0, 0)
 var places = {
 			Vector2(0, 0) :[],
 			Vector2(1, 0): ['cookie'],
@@ -28,7 +28,7 @@ var seenbosses = []
 var carrying = [] setget sort_inv
 
 var health = 5 setget update_health
-var energy = 100 setget update_energy
+var energy = 5 setget update_energy
 var damage = 0
 var max_health = 5 setget update_maxhealth
 var defeated = false
